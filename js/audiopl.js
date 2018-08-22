f8952d176e75e3340ec86ce883566b8c05ca9eb1
id github: radianteyed <radiant.eyed@gmail.com>
2018-08-22 10:38:11 +0200

    added audio contols

--- js/audiopl.js
+++ js/audiopl.js
@@ -1,13 +1,40 @@
 
 
-var audio, playbtn, seek_bar;
+var audio, playbtn, mutebtn,seek_bar;
 
 function initAudioPlayer() {
     audio = new Audio;
     audio.src = "http://stream2.nadaje.com:13332/offradiokrakow";
     audio.autoplay = true;
+    audio.play();
    
+    playbtn = document.getElementById("playpausebtn");
+    mutebtn = document.getElementById("mutebtn");
 
+    playbtn.addEventListener("click", playPause);
+    mutebtn.addEventListener("click", mute);
+
+    
+            
+
+    function playPause() {
+        if (audio.paused) {
+            audio.play();
+            playbtn.style.background = "url(img/pause-button.png) no-repeat";
+        } else {
+            audio.pause();
+            playbtn.style.background = "url(img/play-button.png) no-repeat";
+        }
+    }
+    function mute() {
+        if (audio.muted) {
+            audio.muted = false;
+            mutebtn.style.background = "url(img/speaker.png) no-repeat";
+        } else {
+            audio.muted = true;
+            mutebtn.style.background = "url(img/volume-off.png) no-repeat";
+        }
+    }
   
 }
 
